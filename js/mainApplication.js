097a77a32770cd4bef6df2cc7a7e019f8a01f56a
Flavio Ruiz <scottap302@gmail.com>
2015-02-16 22:21:51 -0430

    Dandole retoques a la aplicacion

--- js/mainApplication.js
+++ js/mainApplication.js
@@ -15,5 +15,15 @@ $(function() {
 	$(".employees").focusout(function(){
 		$(".form-for-employees").fadeOut();
 	});
-	
+
+	$("#btn_query").click(function(){
+		$("#query").toggle("slow");	
+		if ($("#btn_query").html() == "")
+		{
+			$("#btn_query").html("Buscar").removeClass("icon-search").addClass("btn btn-success");
+		}
+		else {
+			$("#btn_query").html("").removeClass("btn btn-success").addClass("icon-search");
+		} 
+	});
 });
